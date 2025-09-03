e946da81d3011ac8a9a59047be42c527bc214e69
sharareh <Sharareh.yahyaei@gmail.com>
2025-09-03 10:34:18 +0200

    add service package and create all crud methods

---
+++ src/main/java/org/example/userapplication/controller/UserController.java
@@ -0,0 +1,53 @@
+package org.example.userapplication.controller;
+
+
+import org.example.userapplication.model.User;
+import org.example.userapplication.repository.UserRepositoy;
+import org.example.userapplication.service.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController("/user")
+public class UserController {
+
+    private final UserRepositoy userRepositoy;
+    private UserService userService;
+
+    public UserController(UserService userService, UserRepositoy userRepositoy) {
+        this.userService = userService;
+        this.userRepositoy = userRepositoy;
+    }
+
+    @GetMapping("/getAllUsers")
+    public ResponseEntity<List<User>> users() {
+        return ResponseEntity.ok(userService.findAllUsers());
+    }
+
+    @PostMapping("/createUser")
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        return ResponseEntity.ok(userService.createUser(user));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUser(@PathVariable Long id) {
+        return ResponseEntity.ok(userService.findUserById(id));
+    }
+
+    @PutMapping("updateUser/{id}")
+    public ResponseEntity<User> updateUser(@PathVariable Long id, @RequestBody User user) {
+        return ResponseEntity.ok(userService.updateUser(id, user));
+
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<String> deleteUser(@PathVariable Long id) {
+        return ResponseEntity.ok(userService.deleteUserById(id));
+    }
+
+    @GetMapping("{email}")
+    public ResponseEntity<User> getUserByEmail(@PathVariable String email) {
+        return ResponseEntity.ok(userService.findUserByEmail(email));
+    }
+}
