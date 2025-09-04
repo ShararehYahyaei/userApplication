2b1b965c084f6486484b2d4cf8961b362d079472
sharareh <Sharareh.yahyaei@gmail.com>
2025-09-04 14:00:41 +0200

    add update method and test it in test repository

--- src/test/java/org/example/userapplication/repository/UseRepositoryTest.java
+++ src/test/java/org/example/userapplication/repository/UseRepositoryTest.java
@@ -10,6 +10,7 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.assertj.core.api.Assertions;
 
 import java.util.List;
+import java.util.Optional;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
@@ -70,6 +71,48 @@ public class UseRepositoryTest {
         userRepository.findById(userSave.getId());
         Assertions.assertThat(userSave).isNotNull();
         Assertions.assertThat(userSave.getId()).isEqualTo(user.getId());
+    }
+
+
+    @Test
+
+    public void userRepository_FIndByEmail_ShouldReturnUser() {
+        User user = User.builder()
+                .firstName("ali")
+                .lastName("chen")
+                .email("chen@gmail.com")
+                .password("123456")
+                .build();
+
+        User userSave = userRepository.save(user);
+        Assertions.assertThat(userSave).isNotNull();
+        Assertions.assertThat(userSave.getId()).isGreaterThan(0);
+        Assertions.assertThat(userSave.getEmail()).isEqualTo("chen@gmail.com");
+
+    }
+
+    @Test
+
+
+    public void userRepository_UpdateUser_ShouldReturnUser() {
+        User user = User.builder()
+                .firstName("ali")
+                .lastName("chen")
+                .email("chen@gmail.com")
+                .password("123456")
+                .build();
+        User userSave = userRepository.save(user);
+        User userNew = userRepository.findById(userSave.getId()).get();
+        userNew.setFirstName("mohammad");
+        userNew.setLastName("chenniii");
+        userNew.setEmail("chennnn@gmail.com");
+        userNew.setPassword("25632");
+        User save = userRepository.save(userNew);
+        Assertions.assertThat(save.getFirstName()).isNotNull();
+        Assertions.assertThat(save.getLastName()).isNotNull();
+        Assertions.assertThat(save.getEmail()).isNotNull();
+        Assertions.assertThat(save.getFirstName()).isEqualTo("mohammad");
+
     }
 }
 
